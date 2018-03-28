976f1d47a4d93b2f6bd3a35cd81c6d264404c5d7
Aaron Montoya <aatoya7@gmail.com>
2018-03-28 13:23:00 -0600

    Added imports and data members for the DrawingCanvas class boy

--- src/art/view/DrawingCanvas.java
+++ src/art/view/DrawingCanvas.java
@@ -1,6 +1,36 @@
 package art.view;
 
-public class DrawingCanvas
-{
+import art.controller.ArtController;
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Polygon;
+import java.awt.Rectangle;
+import java.awt.Shape;
+import java.awt.geom.Ellipse2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 
+import javax.imageio.ImageIO;
+import javax.swing.JFileChooser;
+import javax.swing.JPanel;
+
+public class DrawingCanvas extends JPanel
+{
+	private ArrayList<Polygon> triangleList;
+	private ArrayList<Polygon> polygonList;
+	private ArrayList<Ellipse2D> ellipseList;
+	private ArrayList<Rectangle> rectangleList;
+	private ArtController app;
+	
+	private BufferedImage canvasImage;
+	
+	DrawingCanvas(ArtController app)
+	{
+		super();
+	}
 }
