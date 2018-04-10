8560973a828509a8c693d00afcf80b956a13c5f0
Aaron Montoya <aatoya7@gmail.com>
2018-04-10 13:14:42 -0600

    Adding shapes to the ArtPanel

--- src/art/view/ArtPanel.java
+++ src/art/view/ArtPanel.java
@@ -123,6 +123,48 @@ public class ArtPanel extends JPanel
 		this.add(sliderPanel);
 	}
 	
+	private boolean coinFlip()
+	
+	{
+		return (int) (Math.random() * 2) == 0;
+	}
+	
+	private Polygon createPolygon(int sides)
+	{
+		Polygon currentShape = new Polygon();
+		
+		int originX = (int) (Math.random() * 600);
+		int originY = (int) (Math.random() * 600);
+		
+		for(int index = 0; index < sides; index++)
+		{
+			int minus = coinFlip() ? -1 : 1;
+			int shiftX = (int) (Math.random() * currentScale) * minus;
+			minus = coinFlip() ? - 1 : 1;
+			int shiftY = (int) (Math.random() * currentScale) * minus;
+			currentShape.addPoint(originX + shiftX, originY + shiftY);
+		}
+		
+		return currentShape;
+	}
+	
+	private Rectangle createRectangle()
+	{
+		
+	}
+	
+	private Ellipse2D createEllipse()
+	{
+		Ellipse2D ellipse = new Ellipse2D.Double();
+		
+		int cornerX = (int) (Math.random() * 600);
+		int cornerY = (int) (Math.random() * 600);
+		double width = Math.random() * currentScale + 1;
+		if (coinFlip())
+		{
+			
+		}
+	
 	private void setupLayout()
 	{
 		
