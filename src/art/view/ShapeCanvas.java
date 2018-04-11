03e1fb79750d7c26bc04143400190bb3a75902f5
Aaron Montoya <aatoya7@gmail.com>
2018-04-11 13:13:22 -0600

    Added method stubs

---
+++ src/art/view/ShapeCanvas.java
@@ -0,0 +1,80 @@
+package art.view;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Polygon;
+import java.awt.Rectangle;
+import java.awt.Shape;
+import java.awt.geom.Ellipse2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.swing.JPanel;
+
+import art.controller.ArtController;
+
+public class ShapeCanvas extends JPanel
+{
+	private ArrayList<Polygon> triangleList;
+	private ArrayList<Polygon> polygonList;
+	private ArrayList<Ellipse2D> ellipseList;
+	private ArrayList<Rectangle> rectangleList;
+	private ArtController app;
+	
+	private BufferedImage canvasImage;
+	
+	public ShapeCanvas(Art Controller app)
+	{
+		super();
+		this.app = app;
+		triangleList = new ArrayList<Polygon>();
+		polygonList = new ArrayList<Polygon>();
+		ellipseList = new ArrayList<Ellipse2D>();
+		rectangleList = new ArrayList<Rectangle>();
+		
+		canvasImage = new BufferedImage(600, 600, BufferedImage.TYPE_INT_ARGB);
+		this.setMinimumSize(new Dimension(600, 600));
+		this.setPreferredSize(new Dimension(600, 600));
+		this.setMaximumSize(getPreferredSize());
+	}
+	
+	public void addShape(Shape current)
+	{
+		
+	}
+	
+	public void clear()
+	{
+		
+	}
+	
+	public void changeBackground()
+	{
+		
+	}
+	
+	public void save()
+	{
+		
+	}
+	
+	private Color randomColor()
+	{
+		
+	}
+	
+	private void updateImage()
+	{
+		
+	}
+	
+	@Override
+	protected void paintComponent(Graphics graphics)
+	{
+		super.paintComponent(graphics);
+		graphics.drawImage(canvasImage, 0, 0, null);
+	}
+}
